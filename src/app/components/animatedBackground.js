ca6496598cfc7ca5c0fe35ea0c68aeb939797a48
unknown <smosh360@hotmail.com>
2023-11-21 19:11:36 +0100

    firsit

---
+++ src/app/components/animatedBackground.js
@@ -0,0 +1,25 @@
+
+import React, { useRef, useEffect } from "react";
+
+import "./background.css";
+
+const AnimatedBackground = () => {
+
+
+  return (
+    <div className="background-container noise">
+      <div class="light x1"></div>
+      <div class="light x2"></div>
+      <div class="light x3"></div>
+      <div class="light x4"></div>
+      <div class="light x5"></div>
+      <div class="light x6"></div>
+      <div class="light x7"></div>
+      <div class="light x8"></div>
+      <div class="light x9"></div>
+    </div>
+  );
+};
+
+
+export default AnimatedBackground;
---
+++ src/app/components/navbar.js
@@ -0,0 +1,25 @@
+"use client"
+import React, { useRef, useEffect } from "react";
+
+import "./navbar.css";
+
+const Navbar = () => {
+
+
+  return (
+    <div className="background-container noise">
+      <div class="light x1"></div>
+      <div class="light x2"></div>
+      <div class="light x3"></div>
+      <div class="light x4"></div>
+      <div class="light x5"></div>
+      <div class="light x6"></div>
+      <div class="light x7"></div>
+      <div class="light x8"></div>
+      <div class="light x9"></div>
+    </div>
+  );
+};
+
+
+export default Navbar;
