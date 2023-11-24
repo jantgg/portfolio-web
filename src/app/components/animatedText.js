d343f120e17f627cae344731b7cc2bf5ac9bc272
unknown <smosh360@hotmail.com>
2023-11-24 18:57:35 +0100

    textanimation

--- src/app/components/animatedText.js
+++ src/app/components/animatedText.js
@@ -6,29 +6,23 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import ScrollToPlugin from "gsap/ScrollToPlugin";
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-{/* <AnimatedText 
-    texto="Tu texto aquí" 
-    width="100%" 
-    justifyContent="center" 
-    fontSize="16px" 
-    textAlign="center" 
-/> */}
-
-
 const AnimatedText = ({ 
     texto, 
-    width = '1005', // Valor predeterminado
-    justifyContent = 'center', // Valor predeterminado
-    fontSize = '1vw', // Valor predeterminado
-    textAlign = 'center' // Valor predeterminado
+    width = '100%', // Corregido el valor predeterminado
+    justifyContent = 'center',
+    fontSize = '1vw',
+    textAlign = 'center'
 }) => {
     const linea1Ref = useRef();
     const linea2Ref = useRef();
-  
+
     useEffect(() => {
         const spansLinea1 = linea1Ref.current.children;
         const spansLinea2 = linea2Ref.current.children;
-      
+        // Calcula el delay en función de la longitud del texto
+        const delay = texto.length > 13 ? 0 : 0.03;
+        const duration = texto.length > 13 ? 1.5 : 1;
+
         for (let i = 0; i < spansLinea1.length; i++) {
             gsap.timeline({
                 scrollTrigger: {
@@ -37,42 +31,38 @@ const AnimatedText = ({
                     toggleActions: "play none none none",
                 }
             })
-            .to(spansLinea1[i], { yPercent: -100, opacity: 0, duration: 0.7, delay: i * 0.04 })
-            .fromTo(spansLinea2[i], { yPercent: 100 }, { yPercent: 0, duration: 0.7, delay: i * 0.04 }, 0);
+            .to(spansLinea1[i], { yPercent: -100, opacity: 0, duration: duration, delay: i * delay, ease: "Power1.easeOut" })
+            .fromTo(spansLinea2[i], { yPercent: 100, opacity: 0 }, { yPercent: 0, opacity: 1, duration: duration, delay: i * delay, ease: "Power1.easeOut" }, 0);
         }
     }, [texto]);
-      
-    // Estilos en línea para el div padre
+
     const containerStyles = {
-        width: width, // Establece el ancho
-        justifyContent: justifyContent, // Establece la alineación horizontal
-        fontSize: fontSize, // Establece el tamaño de la fuente
-        textAlign: textAlign // Establece la alineación del texto
+        width: width,
+        justifyContent: justifyContent,
+        fontSize: fontSize,
+        textAlign: textAlign
     };
-        // Función para renderizar el contenido del texto, incluyendo íconos o componentes
-        const renderContent = (content) => {
-            if (typeof content === 'string') {
-                return content.split("").map((char, index) => (
-                    <span key={`line-${index}`}>{char}</span>
-                ));
-            } else {
-                // Aquí asumimos que el contenido es un componente o un elemento JSX
-                return <>{content}</>;
-            }
-        };
-    
+
+    const renderContent = (content) => {
+        if (typeof content === 'string') {
+            return content.split("").map((char, index) => (
+                <span key={`line-${index}`}>{char}</span>
+            ));
+        } else {
+            return <>{content}</>;
+        }
+    };
   
     return (
         <div className="at-container" style={containerStyles}>
-        <div className="at-linea1" ref={linea1Ref}>
-            {renderContent(texto)}
+            <div className="at-linea1" ref={linea1Ref}>
+                {renderContent(texto)}
+            </div>
+            <div className="at-linea2" ref={linea2Ref}>
+                {renderContent(texto)}
+            </div>
         </div>
-        <div className="at-linea2" ref={linea2Ref}>
-            {renderContent(texto)}
-        </div>
-    </div>
-
     );
 };
-  
+
 export default AnimatedText;
